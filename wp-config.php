428c9abbf6e41e00fa010fa4983173744fa13c41
Mario Sommer <mario.sommer@moodley.at>
2013-02-24 12:47:07 +0100

    Updated wp-config

--- wp-config.php
+++ wp-config.php
@@ -17,6 +17,12 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress-core');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+//define('WP_DEFAULT_THEME', 'mytheme');
+
 
 if(file_exists(dirname(__FILE__) . '/local-config.php' )){
     include(dirname(__FILE__). '/local-config.php');
