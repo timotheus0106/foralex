3c81baec7fe0347e17713f6cdb58f8fe0f206da5
Mario Sommer <mario.sommer@moodley.at>
2013-02-25 09:16:11 +0100

    removed settings page has_cap warning

--- wp-content/plugins/muevo-lib/lib/muevoSettingsPage/muevoSettingsPage.php
+++ wp-content/plugins/muevo-lib/lib/muevoSettingsPage/muevoSettingsPage.php
@@ -6,28 +6,28 @@
  */
 
 class muevoSettingsPage extends muevoLib {
-	
+
 	public static function init($args) {
-		
+
 	}
-	
+
  	public function muevoSettingsPage($title, $fields) {
  		$this->title = $title;
 		$this->fields = $fields;
-		
-		$this->standard_fields = array(	
-		
+
+		$this->standard_fields = array(
+
 			"Google Analytics Code" => array( "ID" => array("string", "google_analytics_code", "Google Analytics ID 'UA-123xyz'")	),
-			
+
 			);
-		
-				
+
+
 		$this->fields = array_merge($this->standard_fields, $fields);
-		
+
  		add_action('admin_menu', array(&$this, 'page_info'));
 		add_action('admin_init', array(&$this, 'register_settings'));
  	}
-	
+
 	public function register_settings() {
 		foreach( $this->fields as $section => $settings ) {
 			foreach( $settings as $setting) {
@@ -35,17 +35,17 @@ class muevoSettingsPage extends muevoLib {
 			}
 		}
 	}
-	
+
 	public function page_info() {
-		add_menu_page($this->title, $this->title, 6, 'page-info-show', array(&$this, 'page_info_show'));
+		add_menu_page($this->title, $this->title, 'moderate_comments', 'page-info-show', array(&$this, 'page_info_show'));
 	}
-	
+
 	public function page_info_show() {
 		?>
 		<style type="text/css">
 			td hr {
 				background-color:#eee;
-				color:#eee; 
+				color:#eee;
 				border:none;
 			}
 		</style>
@@ -79,7 +79,7 @@ class muevoSettingsPage extends muevoLib {
 				</p>
 			</form>
 		</div>
-		
+
 		<?php
 	}
 }
