5fca779fed9d9e58e981b59a636bd9aca8efb28f
Marcel Seelig <marcel.seelig@moodley.at>
2014-05-14 17:38:03 +0200

    +picturefill

---
+++ content/themes/mbi-theme/assets/src/js/modules/mbipicturefill.js
@@ -0,0 +1,78 @@
+/**
+ * mbiPicturefill module
+ *
+ * @version 0.1.2
+ */
+define([
+	'jquery',
+	'vendor/picturefill',
+	'vendor/matchmedia'
+], function(
+	$
+) {
+    'use strict';
+
+	var module = {
+
+		loadingOffset: 0.75,
+
+		checkvisible: function(element) {
+
+			var vpH = $(window).height(),
+			st = $(window).scrollTop(),
+			y = $(element).offset().top;
+
+			return (y < (vpH*module.loadingOffset + st));
+
+		},
+		activateVisiblePicturefills: function() {
+
+			var redraw = false;
+
+			$('.picture').each(function(i, element) {
+
+				var needsToDisplay = module.checkvisible(element) && !$(element).attr('data-picture');
+
+				if(needsToDisplay) {
+
+					$(element).attr('data-picture', '');
+					redraw = true;
+
+				}
+
+
+			});
+
+			window.picturefill();
+
+		},
+		init: function(options) {
+
+			if(options !== undefined && options instanceof Object) {
+
+				module.loadingOffset = options.offset;
+
+			}
+
+			$(window).scroll(function() {
+
+				module.activateVisiblePicturefills();
+
+			});
+
+			$(document).ready(function() {
+
+				module.activateVisiblePicturefills();
+
+			});
+
+		}
+
+	}
+
+	module.init({ offset: 0.5});
+
+	return module;
+
+
+});
