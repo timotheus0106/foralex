66f457d9dd6f22a7405a5dd990c2703c2a9f4bb9
timotheus0106 <timotheus.triebl@moodley.at>
2014-06-12 01:11:44 +0200

    startpage

---
+++ content/themes/mbi-theme/start.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Start Page
+ * Template Description: Start page
+ * The Template for displaying the first (start) page.
+ * @package moodley
+ * @subpackage moodley-web
+ * @since mbi-theme 1.0
+ */
+
+get_header(); ?>
+
+    <?php
+        
+    ?>
+
+	<div class="startpage--site-wrapper box">
+		
+		<div class="logo"></div>
+		<div class="startpage--button--wrapper"></div>
+
+	</div>
+
+
+
+<?php get_footer(); ?>
--- wp-local-config.php
+++ wp-local-config.php
@@ -1,6 +1,6 @@
 <?php
 /** Local Dev Config File **/
-define('DB_NAME', 'cryoshelter');
+define('DB_NAME', 'alexprasser');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
