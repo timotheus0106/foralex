0652ce1ad66e4e3f4ac34149b81a949bb1b8d8bd
timotheus0106 <timotheus.triebl@moodley.at>
2014-11-19 19:10:19 +0100

    some changes

--- content/themes/mbi-theme/start.php
+++ content/themes/mbi-theme/start.php
@@ -76,106 +76,7 @@ get_header(); ?>
 <!-- button down END -->
 
 	</div>
-<!-- SLIDER-PAGE: START -->
-	<div class="slider__site-wrapper box">
 
-	<!-- INFO-MENU: START -->
-		<div class="infoWrapper">
-
-			<div class="logo is_visible"></div>
-			<div class="contactData">
-				<div class="contactData-inner">
-					<!-- <div class="name"><?php echo $name; ?></div>
-					<div class="street"><?php echo $street; ?></div>
-					<div class="tel link">T&nbsp;&nbsp;&nbsp;<a href="tel:00436608300211"><?php echo $tel; ?></a></div>
-					<div class="mail link">M&nbsp;&nbsp;&nbsp;<a href="mailto:<?php echo $mail; ?>"><?php echo $mail; ?></a></div> -->
-
-                    <div class="name">ALEXANDER PRASSER</div>
-                    <div class="street">ENGE GASSE 3, 8010 GRAZ</div>
-                    <div class="tel link">T&nbsp;&nbsp;&nbsp;<a href="tel:00436608300211">0043 660 / 8300211</a></div>
-                    <div class="mail link">M&nbsp;&nbsp;&nbsp;<a href="mailto:info@alexanderprasser.at">info@alexanderprasser.at</a></div>
-				</div>
-			</div>
-			<div class="openTimes">
-				<p>	<span class="openTimes__heading">ÖFFNUNGSZEITEN</span><br/>
-					DI-MI: 10 BIS 19 UHR<br/>
-					DO-FR: 10 BIS 20 UHR<br/>
-					SA: 09 BIS 15 UHR
-				</p>
-			</div>
-			<div class="furtherInformation">
-				<p>20% Studentenrabatt von Dienstag bis Donnerstag bis 26 Jahre.
-					Kinder unter 12 Jahren bekommen in der Zeit von Montag bis Freitag jeweils bis 15 Uhr 50% Rabat
-				</p>
-			</div>
-		</div>
-	<!-- INFO-MENU: END -->
-
-	<!-- SWIPER: START -->
-        <div class="module image--wrapper">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-            <!--First Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide1" src="content/themes/mbi-theme/assets/gfx/Img/slide_01_s-s.jpg" alt="slide img">
-                    </div>
-
-            <!--Second Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide2" src="content/themes/mbi-theme/assets/gfx/Img/slide_02_s-s.jpg" alt="slide img">
-                    </div>
-
-            <!--Third Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide3" src="content/themes/mbi-theme/assets/gfx/Img/slide_03_s-s.jpg" alt="slide img">
-                    </div>
-            <!--Fourth Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide4" src="content/themes/mbi-theme/assets/gfx/Img/slide_04_s-s.jpg" alt="slide img">
-                    </div>
-            <!--Fifth Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide5" src="content/themes/mbi-theme/assets/gfx/Img/slide_05_s-s.jpg" alt="slide img">
-                    </div>
-            <!--Sixths Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide6" src="content/themes/mbi-theme/assets/gfx/Img/slide_06_s-s.jpg" alt="slide img">
-                    </div>
-            <!--Seventh Slide-->
-                    <div class="swiper-slide">
-                        <img class="image img__slide7" src="content/themes/mbi-theme/assets/gfx/Img/slide_07_s-s.jpg" alt="slide img">
-                    </div>
-            <!--Etc..-->
-            <!--Etc..-->
-                </div>
-            </div>
-            <div class="swiper__buttons">
-                <div class="button--back js_swiperButton"><?php SVG::getFromFile('swipe__button--left', 'swipe__button--left'); ?></div>
-                <div class="button--forward js_swiperButton"><?php SVG::getFromFile('swipe__button--right', 'swipe__button--right'); ?></div>
-            </div>
-        </div>
-
-    <!-- SWIPER: END -->
-    <!-- BUTTONS BOTTOM: START -->
-        <div class="linksBottom">
-            <div class="bottom__contact link js_contactLink">kontakt</div>
-            <div class="bottom__openTimes link js_openTimes">öffnungszeiten</div>
-            <div class="bottom__discount link js_discount">rabatte</div>
-        </div>
-	<!-- BUTTONS BOTTOM: END -->
-
-    <!-- button down -->
-        <div class="startpage__button--wrapper js_scrollDown">
-            <div class="button--black--down--wrapper">
-                <!--<div class="buttons button--black--down"></div>-->
-                <?php SVG::getFromFile('arrow_down', 'svg__arrow--down') ?>
-            </div>
-        </div>
-<!-- button down END -->
-
-
-	</div>
-<!-- SLIDER-PAGE: END -->
 <!-- BIOGRAPHY-PAGE: START -->
 
     <div class="bio__site-wrapper box">
@@ -216,11 +117,11 @@ get_header(); ?>
         <div class="textbio">
             <div class="headline">Alexander Prasser<br/><span class="subheading">Style Director</span></div>
             <div class="text">
-                <p>Die richtige Mischung aus Zeitgeist und Zeitlosigkeit und dabei gleichzeitig den Wünschen und Vorstellungen seiner Kunden und Kundinnen gerecht zu werden, so lautet ein Credo des Style Directors Alexander Prasser.</p>
+                <p>Die richtige Mischung aus Zeitgeist und Zeitlosigkeit und dabei gleichzeitig den Wünschen und Vorstellungen seiner Kunden/innen gerecht zu werden, so lautet das Credo des Style Directors Alexander Prasser.</p>
 
                 <p>Seine Karriere als Stylist begann Alexander Prasser bei der britischen Salon-Kette Toni&Guy. In den dazugehörigen Academies in England und Deutschland verfeinerte er sein Know-how und hielt später auch selbst Seminare ab. Nach weiteren Stationen als Mitbegründer des innovativen Geschäftskonzepts „Coins“, bei Dieter Ferschinger und Show-Bookings für Toni&Guy auf internationalen Hair & Style-Messen, hat es Alexander Prasser nun in die Selbstständigkeit gezogen.</p>
 
-                <p>Im Salon in der Lange Gasse 3, im Herzen von Graz, unter einem Dach mit Bright Hairdressers, bleibt Alexander Prasser seiner Philosophie treu: „Es ist mir wichtig, meinen Kundinnen und Kunden dabei zu helfen, ihre eigene Identität zu entfalten. Bei allen Trends, stehen bei mir das Individuelle und die Entwicklung des persönlichen Stils immer im Vordergrund.“</p>
+                <p>Im Salon in der Engegasse 3, im Herzen von Graz, unter einem Dach mit Bright Hairdressers, bleibt Alexander Prasser seiner Philosophie treu: „Es ist mir wichtig, meinen Kunden/innen dabei zu helfen, ihre eigene Identität zu entfalten. Bei allen Trends, stehen bei mir das Individuelle und die Entwicklung des persönlichen Stils immer im Vordergrund.“</p>
             </div>
         </div>
     <!-- TEXT-BIO: END -->
@@ -249,8 +150,110 @@ get_header(); ?>
             <div class="bottom__discount link js_discount">rabatte</div>
         </div>
     <!-- BUTTONS BOTTOM: END -->
+    <!-- button down -->
+        <div class="startpage__button--wrapper js_scrollDown">
+            <div class="button--black--down--wrapper">
+                <!--<div class="buttons button--black--down"></div>-->
+                <?php SVG::getFromFile('arrow_down', 'svg__arrow--down') ?>
+            </div>
+        </div>
+<!-- button down END -->
     </div>
 <!-- BIOGRAPHY-PAGE: END -->
+
+<!-- SLIDER-PAGE: START -->
+    <div class="slider__site-wrapper box">
+
+    <!-- INFO-MENU: START -->
+        <div class="infoWrapper">
+
+            <div class="logo is_visible"></div>
+            <div class="contactData">
+                <div class="contactData-inner">
+                    <!-- <div class="name"><?php echo $name; ?></div>
+                    <div class="street"><?php echo $street; ?></div>
+                    <div class="tel link">T&nbsp;&nbsp;&nbsp;<a href="tel:00436608300211"><?php echo $tel; ?></a></div>
+                    <div class="mail link">M&nbsp;&nbsp;&nbsp;<a href="mailto:<?php echo $mail; ?>"><?php echo $mail; ?></a></div> -->
+
+                    <div class="name">ALEXANDER PRASSER</div>
+                    <div class="street">ENGE GASSE 3, 8010 GRAZ</div>
+                    <div class="tel link">T&nbsp;&nbsp;&nbsp;<a href="tel:00436608300211">0043 660 / 8300211</a></div>
+                    <div class="mail link">M&nbsp;&nbsp;&nbsp;<a href="mailto:info@alexanderprasser.at">info@alexanderprasser.at</a></div>
+                </div>
+            </div>
+            <div class="openTimes">
+                <p> <span class="openTimes__heading">ÖFFNUNGSZEITEN</span><br/>
+                    DI-MI: 10 BIS 19 UHR<br/>
+                    DO-FR: 10 BIS 20 UHR<br/>
+                    SA: 09 BIS 15 UHR
+                </p>
+            </div>
+            <div class="furtherInformation">
+                <p>20% Studentenrabatt von Dienstag bis Donnerstag bis 26 Jahre.
+                    Kinder unter 12 Jahren bekommen in der Zeit von Montag bis Freitag jeweils bis 15 Uhr 50% Rabat
+                </p>
+            </div>
+        </div>
+    <!-- INFO-MENU: END -->
+
+    <!-- SWIPER: START -->
+        <div class="module image--wrapper">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+            <!--First Slide-->
+                    <div class="swiper-slide">
+                        <img class="image img__slide1" src="content/themes/mbi-theme/assets/gfx/Img/slide_01_s-s.jpg" alt="slide img">
+                    </div>
+
+            <!--Second Slide-->
+                    <div class="swiper-slide">
+                        <img class="image img__slide2" src="content/themes/mbi-theme/assets/gfx/Img/slide_02_s-s.jpg" alt="slide img">
+                    </div>
+
+            <!--Third Slide-->
+                    <!-- <div class="swiper-slide">
+                        <img class="image img__slide3" src="content/themes/mbi-theme/assets/gfx/Img/slide_03_s-s.jpg" alt="slide img">
+                    </div> -->
+            <!--Fourth Slide-->
+                    <div class="swiper-slide">
+                        <img class="image img__slide4" src="content/themes/mbi-theme/assets/gfx/Img/slide_04_s-s.jpg" alt="slide img">
+                    </div>
+            <!--Fifth Slide-->
+                    <!-- <div class="swiper-slide">
+                        <img class="image img__slide5" src="content/themes/mbi-theme/assets/gfx/Img/slide_05_s-s.jpg" alt="slide img">
+                    </div> -->
+            <!--Sixths Slide-->
+                    <!-- <div class="swiper-slide">
+                        <img class="image img__slide6" src="content/themes/mbi-theme/assets/gfx/Img/slide_06_s-s.jpg" alt="slide img">
+                    </div> -->
+            <!--Seventh Slide-->
+                    <div class="swiper-slide">
+                        <img class="image img__slide7" src="content/themes/mbi-theme/assets/gfx/Img/slide_07_s-s.jpg" alt="slide img">
+                    </div>
+            <!--Etc..-->
+            <!--Etc..-->
+                </div>
+            </div>
+            <div class="swiper__buttons">
+                <div class="button--back js_swiperButton"><?php SVG::getFromFile('swipe__button--left', 'swipe__button--left'); ?></div>
+                <div class="button--forward js_swiperButton"><?php SVG::getFromFile('swipe__button--right', 'swipe__button--right'); ?></div>
+            </div>
+        </div>
+
+    <!-- SWIPER: END -->
+    <!-- BUTTONS BOTTOM: START -->
+        <div class="linksBottom">
+            <div class="bottom__contact link js_contactLink">kontakt</div>
+            <div class="bottom__openTimes link js_openTimes">öffnungszeiten</div>
+            <div class="bottom__discount link js_discount">rabatte</div>
+        </div>
+    <!-- BUTTONS BOTTOM: END -->
+
+
+
+
+    </div>
+<!-- SLIDER-PAGE: END -->
 
 
 
